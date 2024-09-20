b02614e5d762bfd112c6a1519306d9e56631bb76
Jonatan <jonatan.alvarez.c@hotmail.com>
2024-09-20 11:50:48 -0600

    ejercicio1

---
+++ Ejercicios/Ejercico1.py
@@ -0,0 +1,7 @@
+numeros = [4]
+numeros[0]=input("ingrese el primer valor: ")
+numeros[1]=input("ingrese el segundo valor: ")
+numeros[2]=input("ingrese el tercero valor: ")
+numeros[3]=input("ingrese el cuarto valor: ")
+
+print(numeros[::-1])
